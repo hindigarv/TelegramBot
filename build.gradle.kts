05e3b435afa29b95a08fc47e1527d870244d6bb0
hindigarv <hindi.garv.001@gmail.com>
2022-12-07 09:07:30 +0100

    upgrade shabdkosh version to 1.0.3 and kotlin to 1.7.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
-//    id("org.jetbrains.kotlin.jvm") version "1.5.31" // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
+    kotlin("jvm") version "1.7.20"
     application
     idea
-    kotlin("jvm") version "1.5.31"
 }
 
 repositories {
@@ -13,7 +12,7 @@ repositories {
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom")) // Align versions of all Kotlin components
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8") // Use the Kotlin JDK 8 standard library.
-    implementation("io.github.hindigarv:shabdkosh:0.2.0")
+    implementation("io.github.hindigarv:shabdkosh:1.0.3")
     implementation("org.telegram:telegrambots:6.0.1")
     implementation("org.slf4j:slf4j-simple:1.7.36")
     testImplementation("org.jetbrains.kotlin:kotlin-test") // Use the Kotlin test library.
